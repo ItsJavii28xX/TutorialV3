70f84670d2f6d4e8ab9a9cc92d18e2a35a61d8da
Javier Rodríguez <frodsot2402@g.educaand.es>
2024-10-03 11:58:58 +0200

    TutorialV2Finished

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Joke } from '../joke';
 import { CommonModule } from '@angular/common';
+import { JokeItemComponent } from '../joke-item/joke-item.component';
+
 
 @Component({
   selector: 'joke-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, JokeItemComponent],
   templateUrl: './joke-list.component.html',
   styleUrl: './joke-list.component.css'
 })
@@ -18,4 +20,5 @@ export class JokeListComponent {
     this.array = [new Joke("Broma1", "Respuesta1"), new Joke("Broma2", "Respuesta2")]
     
   }
+  
 }
